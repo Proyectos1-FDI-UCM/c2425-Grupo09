e3e36fbe4257902c8c4de88ace5cf1b79c8298f1
Valley2018 <valeyu123@gmail.com>
2025-02-23 20:36:06 +0100

    Timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 using TMPro;
 
 public class Timer : MonoBehaviour
@@ -6,6 +8,9 @@ public class Timer : MonoBehaviour
     [SerializeField] public TextMeshProUGUI TimerText;
 
     [SerializeField] float time = 600f;
+    [SerializeField] float SumaSeg = 5f;
+    [SerializeField] float RestaSeg = 5f;
+
     static float currenttime;
     static bool playing = true;
     public Animator animator;
@@ -26,12 +31,21 @@ public class Timer : MonoBehaviour
             currenttime -= Time.deltaTime;
             int minutes = Mathf.FloorToInt(currenttime / 60);
             int seconds = Mathf.FloorToInt(currenttime % 60);
+
+            if (minutes <= 0 && seconds <= 0)
+            {
+                minutes = 0;
+                seconds = 0;
+                TimerText.text = string.Format("{0:00}:{1:00}", minutes, seconds); StopCounting();
+            }
             TimerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+            if (Keyboard.current.rKey.wasPressedThisFrame) { currenttime = currenttime + SumaSeg; }
+            if (Keyboard.current.eKey.wasPressedThisFrame) { currenttime = currenttime - RestaSeg; }
 
             if (currenttime > (time / 3) * 2) { animator.SetBool("1", true); }
             if (currenttime < (time / 3) * 2 && currenttime > (time / 3)) { animator.SetBool("2", true);  }
             //if (currenttime < (time / 3)) { animator.SetBool("3", true); }
-            if (minutes == 0 && seconds == 0) { StopCounting(); }
+           
 
         }
     }
