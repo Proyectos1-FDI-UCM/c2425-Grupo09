24bf2fee73a3623b943fed414fc394b8f19c90be
Sergio Valiente <sevalien@ucm.es>
2025-02-23 17:30:37 +0100

    Disparo y colisión

--- Assets/Scripts/Testing/Player/Bullet.cs
+++ Assets/Scripts/Testing/Player/Bullet.cs
@@ -1,76 +1,72 @@
 //---------------------------------------------------------
-// Componente de test para probar el input de disparo 
-// Guillermo Jiménez Díaz
-// TemplateP1
+// Script que lleva la bala para gestionar su movimiento
+// Sergio Valiente Urueña
+// The Last Vessel
 // Proyectos 1 - Curso 2024-25
 //---------------------------------------------------------
 
 using UnityEngine;
+// Añadir aquí el resto de directivas using
+
 
 /// <summary>
-/// Componente de prueba que se comunica con el InputManager
-/// para mostrar por consola los eventos de la acción Fire.
-/// Como los eventos IsPressed se muestran cada frame y
-/// saturan la consola, tenemos un tick en el editor para
-/// habilitarlos
+/// Antes de cada class, descripción de qué es y para qué sirve,
+/// usando todas las líneas que sean necesarias.
 /// </summary>
-public class TestFire : MonoBehaviour
+public class Bullet : MonoBehaviour
 {
     // ---- ATRIBUTOS DEL INSPECTOR ----
-
     #region Atributos del Inspector (serialized fields)
-    
-    /// <summary>
-    /// Si está activado, se muestran todos los eventos de que
-    /// la acción está siendo realizada (uno por frame)
-    /// </summary>
-    [SerializeField]
-    private bool displayIsPressed = false;
+    // Documentar cada atributo que aparece aquí.
+    // El convenio de nombres de Unity recomienda que los atributos
+    // públicos y de inspector se nombren en formato PascalCase
+    // (palabras con primera letra mayúscula, incluida la primera letra)
+    // Ejemplo: MaxHealthPoints
+
+    [SerializeField] private float speed;
 
     #endregion
-
-
-
+    
     // ---- ATRIBUTOS PRIVADOS ----
-
     #region Atributos Privados (private fields)
-
     // Documentar cada atributo que aparece aquí.
     // El convenio de nombres de Unity recomienda que los atributos
     // privados se nombren en formato _camelCase (comienza con _, 
     // primera palabra en minúsculas y el resto con la 
     // primera letra en mayúsculas)
     // Ejemplo: _maxHealthPoints
+    private Rigidbody2D rb;
 
     #endregion
-
+    
     // ---- MÉTODOS DE MONOBEHAVIOUR ----
-
     #region Métodos de MonoBehaviour
+    
+    // Por defecto están los típicos (Update y Start) pero:
+    // - Hay que añadir todos los que sean necesarios
+    // - Hay que borrar los que no se usen 
+    
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before 
+    /// any of the Update methods are called the first time.
+    /// </summary>
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * speed;
+    }
 
     /// <summary>
     /// Update is called every frame, if the MonoBehaviour is enabled.
     /// </summary>
     void Update()
     {
-        if (InputManager.Instance.FireWasPressedThisFrame())
-            Debug.Log($"{Time.frameCount}[{Time.deltaTime}]: Fire was pressed this frame");
-
-        if (InputManager.Instance.FireWasReleasedThisFrame())
-            Debug.Log($"{Time.frameCount}[{Time.deltaTime}]: Fire was released this frame");
-
-        if (displayIsPressed && InputManager.Instance.FireIsPressed())
-        {
-            Debug.Log($"{Time.frameCount}[{Time.deltaTime}]: Fire was pressed");
-        }
+        
     }
-
     #endregion
 
     // ---- MÉTODOS PÚBLICOS ----
-
     #region Métodos públicos
-
     // Documentar cada método que aparece aquí con ///<summary>
     // El convenio de nombres de Unity recomienda que estos métodos
     // se nombren en formato PascalCase (palabras con primera letra
@@ -78,20 +74,20 @@ public class TestFire : MonoBehaviour
     // Ejemplo: GetPlayerController
 
     #endregion
-
+    
     // ---- MÉTODOS PRIVADOS ----
-
     #region Métodos Privados
-
     // Documentar cada método que aparece aquí
     // El convenio de nombres de Unity recomienda que estos métodos
     // se nombren en formato PascalCase (palabras con primera letra
     // mayúscula, incluida la primera letra)
-    private void Shoot()
+
+    private void OnCollisionEnter2D(Collision2D coll)
     {
-        
+        Destroy(gameObject);
     }
 
-    #endregion
-} // class TestFire 
+    #endregion   
+
+} // class Bullet 
 // namespace
--- Assets/Scripts/Testing/Player/Weapon.cs
+++ Assets/Scripts/Testing/Player/Weapon.cs
@@ -1,76 +1,76 @@
 //---------------------------------------------------------
-// Componente de test para probar el input de disparo 
-// Guillermo Jiménez Díaz
-// TemplateP1
+// Script que se encarga de gestionar el disparo
+// Sergio Valiente Urueña
+// The Last Vessel
 // Proyectos 1 - Curso 2024-25
 //---------------------------------------------------------
 
 using UnityEngine;
+// Añadir aquí el resto de directivas using
+
 
 /// <summary>
-/// Componente de prueba que se comunica con el InputManager
-/// para mostrar por consola los eventos de la acción Fire.
-/// Como los eventos IsPressed se muestran cada frame y
-/// saturan la consola, tenemos un tick en el editor para
-/// habilitarlos
+/// Antes de cada class, descripción de qué es y para qué sirve,
+/// usando todas las líneas que sean necesarias.
 /// </summary>
-public class TestFire : MonoBehaviour
+public class Weapon : MonoBehaviour
 {
     // ---- ATRIBUTOS DEL INSPECTOR ----
-
     #region Atributos del Inspector (serialized fields)
-    
-    /// <summary>
-    /// Si está activado, se muestran todos los eventos de que
-    /// la acción está siendo realizada (uno por frame)
-    /// </summary>
-    [SerializeField]
-    private bool displayIsPressed = false;
+    // Documentar cada atributo que aparece aquí.
+    // El convenio de nombres de Unity recomienda que los atributos
+    // públicos y de inspector se nombren en formato PascalCase
+    // (palabras con primera letra mayúscula, incluida la primera letra)
+    // Ejemplo: MaxHealthPoints
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private float cadenciaDeDisparo;
 
     #endregion
-
-
-
+    
     // ---- ATRIBUTOS PRIVADOS ----
-
     #region Atributos Privados (private fields)
-
     // Documentar cada atributo que aparece aquí.
     // El convenio de nombres de Unity recomienda que los atributos
     // privados se nombren en formato _camelCase (comienza con _, 
     // primera palabra en minúsculas y el resto con la 
     // primera letra en mayúsculas)
     // Ejemplo: _maxHealthPoints
+    private float tiempoUltimoDisparo;
 
     #endregion
-
+    
     // ---- MÉTODOS DE MONOBEHAVIOUR ----
-
     #region Métodos de MonoBehaviour
+    
+    // Por defecto están los típicos (Update y Start) pero:
+    // - Hay que añadir todos los que sean necesarios
+    // - Hay que borrar los que no se usen 
+    
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before 
+    /// any of the Update methods are called the first time.
+    /// </summary>
+    void Start()
+    {
+        
+    }
 
     /// <summary>
     /// Update is called every frame, if the MonoBehaviour is enabled.
     /// </summary>
     void Update()
     {
-        if (InputManager.Instance.FireWasPressedThisFrame())
-            Debug.Log($"{Time.frameCount}[{Time.deltaTime}]: Fire was pressed this frame");
-
-        if (InputManager.Instance.FireWasReleasedThisFrame())
-            Debug.Log($"{Time.frameCount}[{Time.deltaTime}]: Fire was released this frame");
-
-        if (displayIsPressed && InputManager.Instance.FireIsPressed())
+        if (InputManager.Instance.FireWasPressedThisFrame() && Time.time > tiempoUltimoDisparo + cadenciaDeDisparo)
         {
-            Debug.Log($"{Time.frameCount}[{Time.deltaTime}]: Fire was pressed");
+            Shoot();
+            tiempoUltimoDisparo = Time.time;
         }
+            
     }
-
     #endregion
 
     // ---- MÉTODOS PÚBLICOS ----
-
     #region Métodos públicos
-
     // Documentar cada método que aparece aquí con ///<summary>
     // El convenio de nombres de Unity recomienda que estos métodos
     // se nombren en formato PascalCase (palabras con primera letra
@@ -78,20 +78,22 @@ public class TestFire : MonoBehaviour
     // Ejemplo: GetPlayerController
 
     #endregion
-
+    
     // ---- MÉTODOS PRIVADOS ----
-
     #region Métodos Privados
-
     // Documentar cada método que aparece aquí
     // El convenio de nombres de Unity recomienda que estos métodos
     // se nombren en formato PascalCase (palabras con primera letra
     // mayúscula, incluida la primera letra)
+    
     private void Shoot()
     {
-        
+        Instantiate(bulletPrefab, transform.position, Quaternion.identity);
     }
 
-    #endregion
-} // class TestFire 
+
+
+    #endregion   
+
+} // class Shoot 
 // namespace
