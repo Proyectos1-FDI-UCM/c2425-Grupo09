b0d0be5f495c0c3a281b1db96ac6bd1bf7ba48e8
Sergio Valiente <sevalien@ucm.es>
2025-03-01 08:50:01 +0100

    Health Bar
    He puesto la versión buena del componente health, combinando health con health bar

--- Assets/Scripts/Player/Player Movement/Health.cs
+++ Assets/Scripts/Player/Player Movement/Health.cs
@@ -1,6 +1,6 @@
 //---------------------------------------------------------
 // Mecanica responsable de manejar la vida del jugador, es decir, de Noe.
-// Alejandro García Díaz
+// Alejandro García Díaz, y Sergio González
 // The Last Vessel
 // Proyectos 1 - Curso 2024-25
 //---------------------------------------------------------
@@ -9,8 +9,8 @@ using System;
 using UnityEngine;
 using TMPro;
 using UnityEngine.InputSystem;
-
-
+using UnityEngine.UI;
+using DG.Tweening;
 // Añadir aquí el resto de directivas using
 
 
@@ -21,26 +21,32 @@ using UnityEngine.InputSystem;
 public class Health : MonoBehaviour
 {
     // ---- ATRIBUTOS DEL INSPECTOR ----
-
-
-    // ---- ATRIBUTOS PRIVADOS ----
     [SerializeField] float maxHealth = 100f;
-
-
+    [SerializeField] private Image _healthBarFill;
+    [SerializeField] private float _fillSpeed;
+    [SerializeField] private Gradient _colorGradient;
+    // ---- ATRIBUTOS PRIVADOS ----
+    private float _currentHealth;
+  
     // ---- MÉTODOS DE MONOBEHAVIOUR ----
+    void Start()
+    {
+        _currentHealth = maxHealth;
+    }
+
     private void Update()
     {
         if (InputManager.Instance.FireWasPressedThisFrame()) //TEST -- Comprobar si aumenta la vida
         {
-            maxHealth += 10f;
-            Debug.Log("Vida restante: " +  maxHealth);
+            Updatehealth(10);
+            Debug.Log("Vida restante: " +  _currentHealth);
         }
         if (InputManager.Instance.JumpWasPressedThisFrame()) //TEST -- Comprobar si reduce la vida
         {
-            maxHealth -= 10f;
-            Debug.Log("Vida restante: " + maxHealth);
+            Updatehealth(-10);
+            Debug.Log("Vida restante: " + _currentHealth);
         }
-        if (maxHealth <= 0) //Si la vida llega a 0 se destruye
+        if (_currentHealth <= 0) //Si la vida llega a 0 se destruye
         {
             Debug.Log("Muerto");
             gameObject.SetActive(false);
@@ -52,11 +58,23 @@ public class Health : MonoBehaviour
     // ---- MÉTODOS PÚBLICOS ----
     public void OnConsumable(int healthUp)
     {
-        maxHealth += healthUp;
+        _currentHealth += healthUp;
     }
 
     // ---- MÉTODOS PRIVADOS ----
+    private void Updatehealth(float amount)
+    {
+        _currentHealth += amount;
+        _currentHealth = Mathf.Clamp(_currentHealth, 0f, maxHealth);
+        UpdateHealthBar();
+    }
 
+    private void UpdateHealthBar()
+    {
+        float targetFillAmount = _currentHealth / maxHealth;
+        _healthBarFill.DOFillAmount(targetFillAmount, _fillSpeed);
+        _healthBarFill.color = _colorGradient.Evaluate(targetFillAmount);
+    }
 
 } // class Health 
 // namespace
